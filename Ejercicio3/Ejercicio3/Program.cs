4c36060cb00ff1ecb443de1e860aed2a64b1a528
Jose Martinez <232283282+JoseMM96@users.noreply.github.com>
2025-09-14 23:40:12 -0600

    Carga ejercicio 3 de la unidad 1

---
+++ Ejercicio3/Ejercicio3/Program.cs
@@ -0,0 +1,63 @@
+﻿namespace Ejercicio3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese un numero entero:");
+            string num = Console.ReadLine();
+            int numeroentero;
+            
+
+            if (int.TryParse(num, out numeroentero))
+            {
+            string mensaje = "El numero";
+                if (numeroentero < 0)
+                {
+                    mensaje += " es negativo";
+                }
+                else if (numeroentero == 0)
+                {
+                    mensaje += " es cero";
+                }
+                else
+                {
+                    mensaje += " es positivo";
+                }
+
+
+                if (numeroentero % 2 == 0)
+                {
+                    mensaje += ", es par";
+                    if (numeroentero % 5 == 0)
+                    {
+                        mensaje += ", y es multiplo de 5.";
+                    }
+                    else
+                    {
+                        mensaje += ", y no es multiplo de 5.";
+                    }
+                }
+                else
+                {
+                    mensaje += ", es impar";
+                    if (numeroentero % 5 == 0)
+                    {
+                        mensaje += ", y es multiplo de 5.";
+                    }
+                    else
+                    {
+                        mensaje += ", y no es multiplo de 5.";
+                    }
+                }
+
+                Console.WriteLine(mensaje);
+            }
+            else
+            {
+                Console.WriteLine("Error, numero entero invalido.");
+            }
+
+        }
+    }
+}
