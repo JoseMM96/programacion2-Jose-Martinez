fb3ec4089ad3f98980f3a4a92e270588aceab54d
Jose Martinez <232283282+JoseMM96@users.noreply.github.com>
2025-09-14 23:18:44 -0600

    Carga de Ejercicio 1 de la unidad 1

---
+++ Ejercicio1/Ejercicio1/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Ejercicio1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese su nombre:");
+            string nombre = Console.ReadLine();
+            Console.WriteLine("Ingrese su año de nacimiento:");
+            string año = Console.ReadLine();
+            int añonacimiento;
+            int añoactual = DateTime.Now.Year;
+
+            if (int.TryParse(año, out añonacimiento)) {
+                int edad = añoactual-añonacimiento;
+                Console.WriteLine($"Hola {nombre}, su edad es {edad} años.");
+            }
+            else {
+                Console.WriteLine("Error, año invalido.");
+            }
+        }
+    }
+}
