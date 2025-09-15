989dafaecbd752a324230fc47ead769c6ea74f21
Jose Martinez <232283282+JoseMM96@users.noreply.github.com>
2025-09-14 23:38:23 -0600

    Carga ejercicio 2 de la unidad 1

---
+++ Ejercicio2/Ejercicio2/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Ejercicio2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese la temperatura en grados Celsius:");
+            string temp = Console.ReadLine();
+            double tempC;
+            double tempF;
+
+            if (double.TryParse(temp, out tempC))
+            {
+                tempF = 32 + (tempC * 9) / 5;
+                Console.WriteLine($"La temperatura en grados Fahrenheit es {tempF}");
+            }
+            else
+            {
+                Console.WriteLine("Error, la temperatura ingresada es invalida");
+            }
+        }
+    }
+}
